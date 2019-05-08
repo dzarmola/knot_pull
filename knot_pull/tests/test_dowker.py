1ed0ee4e5a8d304440ac1254b29b60d18b225dc1
dzarmola <dzarmola@gmail.com>
2019-05-08 11:33:00 +0200

    Fixes for travis

--- knot_pull/tests/test_dowker.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-
-import knot_pull
-
-class TestDowker(TestCase):
-    def test_dowker_is_string(self):
-        s = "nothing for now"
-        self.assertTrue(isinstance(s, basestring))
